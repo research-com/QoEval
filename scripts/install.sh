9489d207ab256874abdaa7676c1ac594f87b963e
Lars Wischhof <wischhof@ieee.org>
2021-03-09 21:51:07 +0100

    added packages required for analysis to install script

--- scripts/install.sh
+++ scripts/install.sh
@@ -12,7 +12,7 @@ MONITORING="wireshark wireshark-qt wireshark-doc"
 VIDEO_REC="ffmpeg vlc-bin wmctrl x11-utils"
 REMOTE_ACCESS="openssh-server"
 JAVA="openjdk-8-jre"
-PYTHON="python3-venv python3-pip"
+PYTHON="python3-venv python3-pip python3-tk tshark"
 BROWSER="firefox"
 
 USER="qoe-user"
