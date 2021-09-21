5b75a4ef8a889b30c29583328d64a33e31de45ba
Jan Krahl <krahl.jan@hm.edu>
2021-09-21 09:27:13 +0200

    parameters in treeview right justified

--- qoemu_pkg/gui/parameter_frame.py
+++ qoemu_pkg/gui/parameter_frame.py
@@ -79,11 +79,11 @@ class ParameterFrame(tk.Frame):
         for column in self.tree['columns']:
             self.tree.heading(column, text=column)
             if column == "link":
-                self.tree.column(column, stretch=False, minwidth=120, width=400)
+                self.tree.column(column, stretch=False, minwidth=120, width=400, anchor="e")
             elif column in ['t_init', 'rul', 'rdl', 'dul', 'ddl', 'codec']:
-                self.tree.column(column, stretch=False, minwidth=60, width=60)
+                self.tree.column(column, stretch=False, minwidth=60, width=60, anchor="e")
             else:
-                self.tree.column(column, stretch=False, minwidth=60, width=100)
+                self.tree.column(column, stretch=False, minwidth=60, width=100, anchor="e")
 
         # make leftclick copy possible in treeview
         self.tree.bind('<ButtonRelease-1>', self._tree_copy_click_handler)
