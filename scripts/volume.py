40b5b8999cf078912231aa1f7338ae8269e427dd
Lars Wischhof <wischhof@ieee.org>
2021-09-24 11:25:53 +0200

    volume script updated to ignore non-existent histogram data

--- scripts/volume.py
+++ scripts/volume.py
@@ -42,11 +42,14 @@ def _get_volume_info(input_filename_list: List[str]) -> str:
             match = (matcher.search(output.stderr))
             if match:
                 hist_vol[i] = int(match.group(1))
-                hist_vol_str = hist_vol_str + f"{hist_vol[i]}"
-                if i < 8:
-                    hist_vol_str = hist_vol_str + ", "
-            else:
-                raise RuntimeError('histogram_volume detection failed.')
+            # Note: since not all files report data for all bins, we ignore a non-match
+            # else:
+            #     print(output.stderr)
+            #     raise RuntimeError('histogram_volume detection failed.')
+
+            hist_vol_str = hist_vol_str + f"{hist_vol[i]}"
+            if i < 8:
+                hist_vol_str = hist_vol_str + ", "
 
         result = result + f"{input_filename}, {max_vol}, {mean_vol}, {hist_vol_str}\n"
     return result
@@ -57,6 +60,10 @@ parser.add_argument('inputfile', nargs='*', help='Specifies the path to the file
 
 args = parser.parse_args()
 
-#print(f"Evaluating {args.inputfile}")
-print(HEADER)
-print(_get_volume_info(args.inputfile))
+if len(args.inputfile) < 1:
+    print("Missing required argument!")
+    parser.print_help()
+else:
+    #print(f"Evaluating {args.inputfile}")
+    print(HEADER)
+    print(_get_volume_info(args.inputfile))
