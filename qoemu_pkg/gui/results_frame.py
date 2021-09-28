2cd9d8c802ffbdaced17cea0e4beb7a1c86fb288
Jan Krahl <krahl.jan@hm.edu>
2021-09-28 15:31:58 +0200

    improve results tab GUI

--- qoemu_pkg/gui/results_frame.py
+++ qoemu_pkg/gui/results_frame.py
@@ -33,7 +33,10 @@ class ResultsFrame(tk.Frame):
         self.frame_stimuli = tk.Frame(self) # , bd=0, borderwidth=1, relief=tk.SOLID)
         self.frame_stimuli.pack(side=tk.LEFT, expand=0, fill=tk.Y)
 
-        self.label_stimuli = tk.Label(self.frame_stimuli, text="Stimuli", font=("Arial", 15))
+        sep = ttk.Separator(self, orient=tk.VERTICAL)
+        sep.pack(side=tk.LEFT, expand=0, fill=tk.Y, padx=4)
+
+        self.label_stimuli = tk.Label(self.frame_stimuli, text="Stimuli", font=("Arial", 15), anchor=tk.W)
         self.label_stimuli.pack(side=tk.TOP, fill=tk.X, expand=tk.NO)
 
         self.listbox_stimuli = tk.Listbox(self.frame_stimuli, exportselection=False)
@@ -42,37 +45,43 @@ class ResultsFrame(tk.Frame):
         self.frame_right = tk.Frame(self)
         self.frame_right.pack(side=tk.RIGHT, fill=tk.BOTH, expand=tk.YES)
 
-        self.label_graphs = tk.Label(self.frame_right, text="Graphs", font=("Arial", 15))
+        self.label_graphs = tk.Label(self.frame_right, text="Graphs", font=("Arial", 15), anchor=tk.W)
         self.label_graphs.pack(side=tk.TOP, fill=tk.X, expand=tk.NO)
 
-        self.frame_graphs = tk.Frame(self.frame_right, bd=0, borderwidth=1, relief=tk.SOLID)
+        self.frame_graphs = tk.Frame(self.frame_right)
         self.frame_graphs.pack(side=tk.TOP, fill=tk.BOTH, expand=tk.YES)
 
-        self.label_videos = tk.Label(self.frame_right, text="Videos", font=("Arial", 15))
-        self.label_videos.pack(side=tk.TOP, expand=tk.NO)
+        self.frame_graphs_listbox = tk.Frame(self.frame_graphs)
+        self.frame_graphs_listbox.pack(side=tk.LEFT, fill=tk.BOTH, expand=tk.NO)
+
+        sep = ttk.Separator(self.frame_right, orient=tk.HORIZONTAL)
+        sep.pack(side=tk.TOP, expand=0, fill=tk.X, pady=2)
+
+        self.label_videos = tk.Label(self.frame_right, text="Videos (select up to two)", font=("Arial", 15), anchor=tk.W)
+        self.label_videos.pack(side=tk.TOP, fill=tk.X)
 
         self.frame_videos = tk.Frame(self.frame_right)
-        self.frame_videos.pack(side=tk.TOP)
+        self.frame_videos.pack(side=tk.LEFT)
 
-        self.listbox_graphs = tk.Listbox(self.frame_graphs, exportselection=False)
-        self.listbox_graphs.pack(side=tk.LEFT, anchor=tk.W, fill=tk.Y, expand=tk.YES)
+        self.listbox_graphs = tk.Listbox(self.frame_graphs_listbox, exportselection=False)
+        self.listbox_graphs.pack(side=tk.TOP, anchor=tk.W, fill=tk.Y, expand=tk.YES)
         self.listbox_graphs.bind('<<ListboxSelect>>', self.on_graph_select)
 
         self.listbox_videos = tk.Listbox(self.frame_videos, exportselection=False, selectmode="multiple")
-        self.listbox_videos.pack(side=tk.LEFT)
+        self.listbox_videos.pack(side=tk.TOP)
         self.listbox_videos.bind('<<ListboxSelect>>', self.on_video_select)
 
         self.frame_preview = tk.Frame(self.frame_graphs)
         self.frame_preview.pack(side=tk.TOP)
 
-        self.label_preview = tk.Label(self.frame_preview, bd=0, borderwidth=1, relief=tk.SOLID)
+        self.label_preview = tk.Label(self.frame_preview)
         self.label_preview.pack(side=tk.TOP)
 
-        self.button_open_pdf = tk.Button(self.frame_preview, text="Open PDF", command=self.open_pdf)
-        self.button_open_pdf.pack(side=tk.TOP, fill=tk.X, expand=tk.YES)
+        self.button_open_pdf = tk.Button(self.frame_graphs_listbox, text="Open PDF", command=self.open_pdf)
+        self.button_open_pdf.pack(side=tk.BOTTOM, expand=tk.NO, fill=tk.X)
 
-        self.button_play = tk.Button(self.frame_right, text="Play selected", command=self.open_video_player)
-        self.button_play.pack(side=tk.TOP)
+        self.button_play = tk.Button(self.frame_videos, text="Play selected videos", command=self.open_video_player)
+        self.button_play.pack(side=tk.BOTTOM, fill=tk.X)
 
         self.update()
 
@@ -102,14 +111,17 @@ class ResultsFrame(tk.Frame):
                 self.selected_video_paths.append(video_path)
 
     def open_pdf(self):
-        pdf_path = None
-        for filename in self.filenames:
-            if filename.startswith(self.listbox_stimuli.get(self.listbox_stimuli.curselection())) \
-                    and filename.endswith("_stats_" + self.listbox_graphs.get(self.listbox_graphs.curselection())):
-                pdf_path = os.path.join(self.gui.qoemu_config.video_capture_path.get(), filename)
-                pdf_path = os.path.splitext(pdf_path)[0] + ".pdf"
-        if pdf_path:
-            subprocess.call(('xdg-open', pdf_path))
+        try:
+            pdf_path = None
+            for filename in self.filenames:
+                if filename.startswith(self.listbox_stimuli.get(self.listbox_stimuli.curselection())) \
+                        and filename.endswith("_stats_" + self.listbox_graphs.get(self.listbox_graphs.curselection())):
+                    pdf_path = os.path.join(self.gui.qoemu_config.video_capture_path.get(), filename)
+                    pdf_path = os.path.splitext(pdf_path)[0] + ".pdf"
+            if pdf_path:
+                subprocess.call(('xdg-open', pdf_path))
+        except tk.TclError:
+            pass
 
     def open_video_player(self):
         if len(self.selected_video_paths) > 2 or len(self.selected_video_paths) < 1:
